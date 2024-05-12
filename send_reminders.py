36fc16af99d228d06f9809ec4e8347d9170dc9bb
zahrabateninia <zahrabatenin@gmail.com>
2024-05-12 18:38:45 +0330

    Create message_template(date, title) function

--- send_reminders.py
+++ send_reminders.py
@@ -16,3 +16,21 @@ def dow(date):
     dateobj = datetime.datetime.strftime(date, r"%d/%m/%Y")
     return dateobj.strftime("%A")
 
+def message_template(date, title):
+    message = email.message.EmailMessage()
+    weekday = dow(date)
+    message['Subject'] = f'Meeting reminder: "{title}"'
+    message.set_content(f'''
+Hi all!
+
+This is a qick mail to remind you all that we have a meeting about:
+"{title}"
+the {weekday} {date}.
+
+
+See you there.
+''')
+    return message
+
+
+
