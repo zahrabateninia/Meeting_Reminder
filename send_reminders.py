d67dec7c0d3a98500ebcb1aa32fbbe3f6688ce69
zahrabateninia <zahrabatenin@gmail.com>
2024-05-12 18:52:25 +0330

    Create main() function

--- send_reminders.py
+++ send_reminders.py
@@ -42,3 +42,19 @@ def send_message(message, emails):
         smtp.send_message(message)
     smtp.quit()
     pass
+
+
+def main():
+    if len(sys.argv) < 2:
+        return usage()
+    try:
+        date, title, emails = sys.argv[1].split('|')
+        message = message_template(date, title)
+        send_message(message, emails)
+        print("Successfully sent reminders to:", emails)
+    except Exception as e:
+        print("Failure to send email", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
